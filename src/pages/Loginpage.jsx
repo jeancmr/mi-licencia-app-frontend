3b5f1893968b29a43a05d170a3775560b65d991c
jeancmr <jean.madiedo08@gmail.com>
2025-05-01 15:44:37 -0500

    feat: fetch login request

--- src/pages/Loginpage.jsx
+++ src/pages/Loginpage.jsx
@@ -4,13 +4,34 @@ const Loginpage = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('http://localhost:3000/api/v1/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ correo: email, contrasena: password }),
+      });
+
+      const data = await response.json();
+      if (!response.ok) {
+        throw new Error(data.message || 'Ocurrió un error');
+      }
+      console.log('Login successful:', data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="flex flex-col items-center justify-center h-screen ">
-      <h1 className="text-3xl font-bold mb-4">Login</h1>
-      <form className="bg-indigo-950 p-6 rounded shadow-md w-80">
+      <h1 className="text-3xl font-bold mb-4">Iniciar sesión</h1>
+      <form onSubmit={handleSubmit} className="bg-indigo-950 p-6 rounded shadow-md w-80">
         <div className="mb-4 text-white">
           <label className="block text-amber-50 text-sm font-bold mb-2" htmlFor="email">
-            Email
+            Correo
           </label>
           <input
             type="email"
@@ -23,7 +44,7 @@ const Loginpage = () => {
         </div>
         <div className="mb-6">
           <label className="block text-amber-50 text-sm font-bold mb-2" htmlFor="password">
-            Password
+            Contraseña
           </label>
           <input
             type="password"
@@ -38,19 +59,19 @@ const Loginpage = () => {
           type="submit"
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full cursor-pointer"
         >
-          Login
+          Iniciar sesión
         </button>
       </form>
       <p className="mt-4 text-gray-600">
-        Don't have an account?{' '}
+        ¿No tienes una cuenta?{' '}
         <a href="/register" className="text-blue-500">
-          Register
+          Regístrate
         </a>
       </p>
       <p className="mt-4 text-gray-600">
-        Forgot your password?{' '}
+        Recuperar contraseña?{' '}
         <a href="/forgot-password" className="text-blue-500">
-          Reset it
+          Click aquí
         </a>
       </p>
     </div>
