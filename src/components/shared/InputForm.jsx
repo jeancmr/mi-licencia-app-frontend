3feb829fa2cf4365b3b609a641dc7d060509840e
jeancmr <jean.madiedo08@gmail.com>
2025-05-17 19:02:19 -0500

    refactor: InputForm error refactor

--- src/components/shared/InputForm.jsx
+++ src/components/shared/InputForm.jsx
@@ -1,13 +1,18 @@
-const InputForm = ({ type, placeholder, register, name, required = false, error }) => {
+const InputForm = ({ type, placeholder, register, name, required = false, error, minLength }) => {
   return (
     <>
       <input
         type={type}
         placeholder={placeholder}
-        {...register(name, { required })}
+        {...register(name, 
+          
+          { required: `El campo ${placeholder} es requerido`,
+          minLength
+        }
+        )}
         className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
       />
-      {error && <span className="text-red-500 block">{placeholder} es requerido</span>}
+      {error && <span className="text-red-500 block">{error.message}</span>}
     </>
   );
 };
