af7e77b70cb3d1be2f8e6332ca185ff181d19726
jeancmr <jean.madiedo08@gmail.com>
2025-05-28 20:28:21 -0500

    style: profile page styles updated

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -6,17 +6,14 @@ const Profile = () => {
   } = useAuth();
 
   return (
-    <div className="flex justify-center items-center h-screen bg-gradient-to-r from-blue-500 to-purple-500">
-      <div className="bg-zinc-800 max-w-md w-full p-10 rounded-md">
-        <h1 className="text-2xl font-bold">Perfil</h1>
-
-        <div className="mt-4 text-white">
-          <p className="text-lg">Nombre: {user.nombre}</p>
-          <p className="text-lg">Email: {user.correo}</p>
-          <p className="text-lg">Rol: {user.rol}</p>
-        </div>
+    <section>
+      <h1 className="text-2xl font-bold">Perfil</h1>
+      <div className="mt-4 text-white">
+        <p className="text-lg">Nombre: {user.nombre}</p>
+        <p className="text-lg">Email: {user.correo}</p>
+        <p className="text-lg">Rol: {user.rol}</p>
       </div>
-    </div>
+    </section>
   );
 };
 export default Profile;
