b21ced8fc0da2168a03567ca5cb009a3a2cb9266
jeancmr <jean.madiedo08@gmail.com>
2025-07-18 14:40:51 -0500

    feat: add no enrolled classes message

--- src/user/student/components/EnrollmentList.jsx
+++ src/user/student/components/EnrollmentList.jsx
@@ -3,13 +3,17 @@ import EnrollmentItem from './EnrollmentItem';
 const EnrollmentList = ({ enrollments, onDeleteEnrollment }) => {
   return (
     <div className="grid grid-cols-2 gap-4 overflow-y-auto flex-1 pr-2">
-      {enrollments.map((enrollment) => (
-        <EnrollmentItem
-          key={enrollment.id}
-          enrollment={enrollment}
-          onDeleteEnrollment={onDeleteEnrollment}
-        />
-      ))}
+      {enrollments.length == 0 ? (
+        <p className="col-span-2 text-center text-gray-500">No tienes clases inscritas.</p>
+      ) : (
+        enrollments.map((enrollment) => (
+          <EnrollmentItem
+            key={enrollment.id}
+            enrollment={enrollment}
+            onDeleteEnrollment={onDeleteEnrollment}
+          />
+        ))
+      )}
     </div>
   );
 };
